1a46505ab468e5468a6ae584400db7b58403af57
N3wika <misa.arnostova@seznam.cz>
2018-12-15 22:19:09 +0100

    Optimalizace. HOTOVO

--- Seq.java
+++ Seq.java
@@ -10,39 +10,28 @@ public class Seq {
         int konec = 0;
         String oddelovac = " ";
 
+        int poziceArg = 0;
         if ( "-s".equals( args[0] ) ) {
-            if ( delka == 3 ) {
-                oddelovac = ( args[1] );
-                konec = Integer.parseInt( args[2] );
-            } else if ( delka == 4 ) {
-                oddelovac = ( args[1] );
-                zacatek = Integer.parseInt( args[2] );
-                konec = Integer.parseInt( args[3] );
-            } else if ( delka == 5 ) {
-                oddelovac = ( args[1] );
-                zacatek = Integer.parseInt( args[2] );
-                krok = Integer.parseInt( args[3] );
-                konec = Integer.parseInt( args[4] );
-            }
-        } else {
-            if ( delka == 1 ) {
-                konec = Integer.parseInt( args[0] );
-            } else if ( delka == 2 ) {
-                zacatek = Integer.parseInt( args[0] );
-                konec = Integer.parseInt( args[1] );
-            } else if ( delka == 3 ) {
-                zacatek = Integer.parseInt( args[0] );
-                krok = Integer.parseInt( args[1] );
-                konec = Integer.parseInt( args[2] );
-            }
+            oddelovac = ( args[1] );
+            poziceArg = 2;
+            delka -= 2;
+        }
+        if ( delka == 1 ) {
+            konec = Integer.parseInt( args[poziceArg] );
+        } else if ( delka == 2 ) {
+            zacatek = Integer.parseInt( args[poziceArg] );
+            konec = Integer.parseInt( args[poziceArg + 1] );
+        } else if ( delka == 3 ) {
+            zacatek = Integer.parseInt( args[poziceArg] );
+            krok = Integer.parseInt( args[poziceArg + 1] );
+            konec = Integer.parseInt( args[poziceArg + 2] );
         }
 
-        tiskni( delka, zacatek, krok, konec, oddelovac );
-
+        tiskni( zacatek, krok, konec, oddelovac );
     }
 
 
-    private static void tiskni( int delka, int zacatek, int krok, int konec, String oddelovac ) {
+    private static void tiskni( int zacatek, int krok, int konec, String oddelovac ) {
         if ( krok >= 0 ) {
             for ( int cislo = zacatek; cislo <= konec; cislo += krok ) {
                 System.out.printf( "%d", cislo );
